0be19e5086dc77f6dbf782b087d85def865864de
Szymon Rosinski <szyros90@gmail.com>
2020-11-09 20:27:53 +0000

    Add two functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-printMessage('Zagrałem kamień! Jeśli twój ruch to papier, to wygrywasz!');
-let computerMove = 'kamień' ;
+let computerMove = ' kamień ' ;
 printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem papier! Jeśli twój ruch to nożyce to wygrywasz!');
-let playerMove = 'papier' ;
+let playerMove = ' papier ' ;
 printMessage('Zagrałem' + playerMove + '! Jeśli twoj ruch to nożyce to wygrywasz!');
