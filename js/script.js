c34ec36c3e1ce89fa094d0e8ce33f7b58fee10e9
Szymon Rosinski <szyros90@gmail.com>
2020-11-09 21:19:06 +0000

    add computerMove options

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-let computerMove = ' kamień ' ;
-printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let playerMove = ' papier ' ;
-printMessage('Zagrałem' + playerMove + '! Jeśli twoj ruch to nożyce to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+    computerMove = 'papier';
+} else (randomNumber == 3) {
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
